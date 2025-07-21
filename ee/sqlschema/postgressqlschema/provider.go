113e26a1d27c5f56d12e4fd0b7b90aa64c1dee20
grandwizard28 <vibhupandey28@gmail.com>
2025-07-21 13:06:26 +0530

    refactor: rename support

--- ee/sqlschema/postgressqlschema/provider.go
+++ ee/sqlschema/postgressqlschema/provider.go
@@ -37,9 +37,9 @@ func New(ctx context.Context, providerSettings factory.ProviderSettings, config 
 		fmter:    fmter,
 		settings: settings,
 		operator: sqlschema.NewOperator(fmter, sqlschema.OperatorSupport{
-			SCreateAndDropConstraint:                     true,
-			SAlterTableAddDropColumnIfNotExistsAndExists: true,
-			SAlterTableAlterColumnSetAndDrop:             true,
+			SCreateAndDropConstraint:                        true,
+			SAlterTableAddAndDropColumnIfNotExistsAndExists: true,
+			SAlterTableAlterColumnSetAndDrop:                true,
 		}),
 	}, nil
 }
